c700afc7319a430407a3917afb1e8ce42121c3ff
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-06-11 12:49:24 +0700

    setup colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#001B0A",
+      secondary: "#FFFFFF",
+      white: "#FFFFFF",
+      black: "#000000",
+      textTitle: "#096A2E",
+      buttonPrimary: "#8ABE53",
+      buttonSecondary: "#E0E0E0",
+    },
   },
   plugins: [],
 };
