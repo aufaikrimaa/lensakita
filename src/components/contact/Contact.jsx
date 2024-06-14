fee0a046dba37cf009c7d0472f2049a86b2b8050
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-06-14 17:14:25 +0700

    add icons and contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,44 +1,53 @@
-import { useGSAP } from "@gsap/react";
-import { gsap, Power3 } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import whatsapp from "../../assets/whatsapp.svg";
+import tiktok from "../../assets/tiktok.svg";
+import instagram from "../../assets/instagram.svg";
+import gmail from "../../assets/gmail.svg";
 
-gsap.registerPlugin(ScrollTrigger);
-
-function Contact() {
+function Contact({ isID }) {
   return (
     <div
-      className="relative h-[100vh]"
+      className="relative h-[100vh] px-[5vw] sm:px-[2.5vw]"
       style={{ clipPath: "polygon(0% 0, 100% 0, 100% 100%, 0 100%)" }}
     >
       <div className="relative h-[calc(100vh+100vh)] -top-[100vh]">
         <div className="sticky top-[calc(100vh-100vh)] h-[100vh]">
           <div className="part8 w-full h-[100vh] bg-secondary flex items-center justify-center relative">
             <div className="w-full flex flex-col items-center">
-              <div className=" flex flex-col items-center gap-[2vw]">
-                <div className="text-[12vw] sm:text-[8vw] tracking-tighter sm:leading-[8vw]">
+              <div className=" flex flex-col items-center gap-[1.5vw]">
+                <div className="text-[12vw] sm:text-[8vw] tracking-tighter sm:leading-[8vw] mb-[4rem]">
                   <img
                     src="https://lensakita-images.vercel.app/images/logo-lk-green.png"
-                    className=" h-[8rem]"
+                    className="h-[4rem] sm:h-[8rem]"
                   />
                 </div>
                 <div className="text-[4vw] mb-[4vw] sm:text-[1.5vw] text-center tracking-tighter sm:mb-[1vw]">
-                  <div className="footPtag overflow-hidden">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    </p>
-                  </div>
-                  <div className="footPtag overflow-hidden">
-                    <p>
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua.
-                    </p>
+                  <div className="footPtag overflow-hidden font-semibold text-gray">
+                    <p>Contact & Social Media</p>
                   </div>
                 </div>
-                <button
+                <div className="flex gap-6">
+                  <img
+                    src={instagram}
+                    className="h-12 w-auto hover:scale-125 transition ease-in-out cursor-pointer"
+                  />
+                  <img
+                    src={tiktok}
+                    className="h-12 w-auto hover:scale-125 transition ease-in-out cursor-pointer"
+                  />
+                  <img
+                    src={whatsapp}
+                    className="h-12 w-auto hover:scale-125 transition ease-in-out cursor-pointer"
+                  />
+                  <img
+                    src={gmail}
+                    className="h-11 w-auto hover:scale-125 transition ease-in-out cursor-pointer"
+                  />
+                </div>
+                {/* <button
                   color="bg-primary"
                   text="Book a demo"
                   color2="text-secondary"
-                />
+                /> */}
               </div>
               <h5 className="absolute bottom-10 tracking-tighter">
                 © LensaKita 2024
