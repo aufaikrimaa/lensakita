c8962fcbeeaeb0bd631a5d02c2e71de4d8cd5552
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-06-25 16:33:42 +0700

    update responsive

--- src/components/packs/Packs.jsx
+++ src/components/packs/Packs.jsx
@@ -32,23 +32,23 @@ function Packs({ isID }) {
   const breakpointColumnsObj = {
     default: 3,
     1100: 3,
-    700: 2,
+    700: 3,
   };
 
   return (
     <div
       ref={container}
-      className="service section sm:w-full  px-8 sm:px-8 mt-32 bg-primary"
+      className="service section sm:w-full px-[5vw] sm:px-8 mt-32 bg-primary"
     >
       <div className="cont h-[300vh] sm:h-[500vh] relative text-white">
         <div className="slides w-full h-[110vh] overflow-hidden sticky top-0 left-0 flex">
           {/* 1st slide */}
-          <div className="slide mt-8 h-screen">
-            <div className="text1 sm:w-[34rem] w-[12rem] text-[2.8rem] leading-[8vh] sm:text-[4rem] sm:leading-[18vh] text-gray mb-6">
+          <div className="slide sm:mt-8 mt-14 h-screen">
+            <div className="text1 sm:w-[34rem] w-full text-[2.8rem] leading-[8vh] sm:text-[4rem] sm:leading-[18vh] text-gray mb-6">
               <h1 className="">{isID ? "Layanan Kami" : "Our Service"}</h1>
             </div>
             <div className="flex sm:flex items-center">
-              <div className="flex flex-wrap gap-x-2  sm:w-[138rem] w-[50rem]">
+              <div className="flex flex-wrap sm:gap-x-2  sm:w-[138rem] w-[46rem]">
                 <div>
                   <Masonry
                     breakpointCols={breakpointColumnsObj}
@@ -56,7 +56,7 @@ function Packs({ isID }) {
                     columnClassName="my-masonry-grid_column"
                   >
                     {schoolEvent.map((item, i) => (
-                      <div key={i} className="w-[60%] sm:w-40 m-1 sm:m-2">
+                      <div key={i} className="w-[7rem] sm:w-40 m-1 sm:m-2">
                         <img src={item.img} className="rounded-md" />
                         <ReactPlayer
                           playing={true}
@@ -70,12 +70,12 @@ function Packs({ isID }) {
                       </div>
                     ))}
                   </Masonry>
-                  <div className="text-center text-3xl font-semibold mb-4">
+                  <div className="text-center sm:text-3xl font-semibold mb-4">
                     School Events
                   </div>
                 </div>
                 <div>
-                  <div className="text-center text-3xl font-semibold mb-4">
+                  <div className="text-center sm:text-3xl font-semibold mb-4">
                     Jeep Tour & Vacation
                   </div>
                   <Masonry
@@ -84,7 +84,7 @@ function Packs({ isID }) {
                     columnClassName="my-masonry-grid_column"
                   >
                     {tour.map((item, i) => (
-                      <div key={i} className="w-[60%] sm:w-40 m-1 sm:m-2">
+                      <div key={i} className="w-[7rem] sm:w-40 m-1 sm:m-2">
                         <img src={item.img} className="rounded-md" />
                         <ReactPlayer
                           playing={true}
@@ -106,7 +106,7 @@ function Packs({ isID }) {
                     columnClassName="my-masonry-grid_column"
                   >
                     {graduation.map((item, i) => (
-                      <div key={i} className="w-[60%] sm:w-40 m-1 sm:m-2">
+                      <div key={i} className="w-[7rem] sm:w-40 m-1 sm:m-2">
                         <img src={item.img} className="rounded-md" />
                         <ReactPlayer
                           playing={true}
@@ -120,12 +120,12 @@ function Packs({ isID }) {
                       </div>
                     ))}
                   </Masonry>
-                  <div className="text-center text-3xl font-semibold mb-4">
+                  <div className="text-center sm:text-3xl font-semibold mb-4">
                     Graduation
                   </div>
                 </div>
                 <div>
-                  <div className="text-center text-3xl font-semibold mb-4">
+                  <div className="text-center sm:text-3xl font-semibold mb-4">
                     Wedding, Pre Wedding & Engagement
                   </div>
                   <Masonry
@@ -134,7 +134,7 @@ function Packs({ isID }) {
                     columnClassName="my-masonry-grid_column"
                   >
                     {wd.map((item, i) => (
-                      <div key={i} className="w-[60%] sm:w-40 m-1 sm:m-2">
+                      <div key={i} className="w-[7rem] sm:w-40 m-1 sm:m-2">
                         <img src={item.img} className="rounded-md" />
                         <ReactPlayer
                           playing={true}
@@ -157,7 +157,7 @@ function Packs({ isID }) {
                     : "For any event and your needs, you can contact us for documentation."}
                 </h3>
                 <div className="packs-link text-[1rem] sm:text-[1.5rem] transition-colors duration-300 hover:text-white w-[22rem] sm:w-full">
-                  <Link to="/">
+                  <Link to="/pricelist">
                     {isID ? "Lihat selengkapnya " : "View more "}
                     <ion-icon name="arrow-forward-circle-sharp"></ion-icon>
                   </Link>
